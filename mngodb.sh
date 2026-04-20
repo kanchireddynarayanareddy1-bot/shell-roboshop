d6c99b37873dd64715039e8de89934a024055cb5
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-20 13:09:21 +0530

    hello

---
+++ mngodb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+UserId=$(id -u) 
+R="\e[031m"
+G="\e[032m"
+Y="\e[033m"
+N="\e[0m"
+
+Folder="/var/log/shell-roboshop"
+ScriptName=$(echo $0 | cut -d'.' -f1)
+LogFile="$Folder/$ScriptName.log"  
+ 
+mkdir -p $Folder
+echo "Script execution started at: $(date)" | tee -a $LogFile   
+
+if [ $UserId -ne 0 ]; then
+    echo "you can give sudo access to this script to run as root user" | tee -a $LogFile
+fi
+validate (){
+   if [ $1 -ne 0 ]; then
+    echo -e " $2 $R failed $N" | tee -a $LogFile
+else
+    echo -e "$2 $G successful $N" | tee -a $LogFile
+fi 
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
