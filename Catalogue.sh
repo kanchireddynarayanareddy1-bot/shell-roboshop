8599baeb300e20bb5afffd93323a4ca64c94e99f
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-21 10:47:20 +0530

    hello

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,63 +1,77 @@
 #!/bin/bash
 
-UserId=$(id -u) 
-R="\e[031m"
-G="\e[032m"
-Y="\e[033m"
-N="\e[0m"   
-Folder="/var/log/shell-roboshop"
-ScriptName=$(echo $0 | cut -d'.' -f1)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-LogFile="$Folder/$ScriptName.log"   
-mkdir -p $Folder
-echo "Script execution started at: $(date)" | tee -a $LogFile       
-if [ $UserId -ne 0 ]; then
-    echo "you can give sudo access to this script to run as root user" | tee -a $LogFile
-    exit 1
+MONGODB_HOST=mongodb.chandrahasa.online
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
- VALIDATE (){
-    if [$1 -ne 0]; then
-        echo -e " $2 $R FAILURE $N" | tee -a $LogFile
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 $G SUCCESS $N" | tee -a $LogFile
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
- }
+}
 
-dnf module disable nodejs -y &>>$LogFile
-VALIDATE $? "Disable NodeJS module"
-
-dnf module enable nodejs:20 -y &>>$LogFile
-VALIDATE $? "Enable NodeJS 20 module"
-
-dnf install nodejs -y &>>$LogFile
+##### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-VALIDATE $? "Adding roboshop user"
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app &>>$LogFile
-VALIDATE $? "Creating application directory"    
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-VALIDATE $? "Downloading application code"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue application"
 
-cd /app
-VALIDATE $? "Changing directory to /app"   
+cd /app 
+VALIDATE $? "Changing to app directory"
 
-rm -rf /app/* &>>$LogFile
-VALIDATE $? "Cleaning old application content"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-unzip /tmp/catalogue.zip &>>$LogFile
-VALIDATE $? "Extracting application code"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip catalogue"
 
-npm install &>>$LogFile
-VALIDATE $? "Installing NodeJS dependencies"
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
 
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying systemd service file"
+VALIDATE $? "Copy systemctl service"
 
-systemctl daemon-reload &>>$LogFile
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
